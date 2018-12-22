3984c33a784dd8cfd4596e16d9cae1acb5c560ec
Saebbi <30954683+Saebbi@users.noreply.github.com>
2018-12-22 14:37:56 +0100

    Added some stuff

---
+++ Sources/CompProperties_LightSustenance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Verse;
+
+namespace Mutations
+{
+    // Token: 0x0200000A RID: 10
+    public class CompProperties_LightSustenance : CompProperties
+    {
+        // Token: 0x0600000F RID: 15
+        public CompProperties_LightSustenance()
+        {
+            this.compClass = typeof(CompLightSustenance);
+        }
+    }
+}
