a9a3fd59e5cdec0ca6895c525cb44bef2b94bdad
Mlie <emipa606@users.noreply.github.com>
2024-04-21 20:57:03 +0200

    Mod updated for 1.5 and passed autotests

--- Source/Regeneration/Ent/HealingFactorProperties.cs
+++ Source/Regeneration/Ent/HealingFactorProperties.cs
@@ -4,16 +4,15 @@ namespace Ent;
 
 public class HealingFactorProperties : DefModExtension
 {
+    public readonly float healWoundFactorMax = 5f;
+
+    public readonly float healWoundFactorMin = 0.01f;
+
+    public readonly int ticksBetweenMajorHeal = 2000;
+
+    public readonly int ticksBetweenMinorHeal = 20;
     public int healOldWoundStage;
-
-    public float healWoundFactorMax = 5f;
-
-    public float healWoundFactorMin = 0.01f;
 
     public int regrowBodypartStage;
     public int tendWoundStage;
-
-    public int ticksBetweenMajorHeal = 2000;
-
-    public int ticksBetweenMinorHeal = 20;
 }
--- Source/VariableBleedRate/VariableBleedRate/Main.cs
+++ Source/VariableBleedRate/VariableBleedRate/Main.cs
@@ -10,7 +10,6 @@ internal static class Main
     static Main()
     {
         Log.Message("VBRF :: Patching TotalBleedRate");
-        var val = new Harmony("com.rate.bleed");
-        val.PatchAll(Assembly.GetExecutingAssembly());
+        new Harmony("com.rate.bleed").PatchAll(Assembly.GetExecutingAssembly());
     }
 }
